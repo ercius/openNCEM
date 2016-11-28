0cb1beae55e6f6a86c9a01dbab6b7a3992742aa5
Florian Niekiel <florian.niekiel@niemail.org>
2016-11-28 10:43:40 -0800

    releasing 1.0.0

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='electron microscopy image processing',
+    keywords='electron microscopy image diffraction processing simulation',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
