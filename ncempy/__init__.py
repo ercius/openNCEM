79a3d875361b940e4b842564c7487f07407a2370
Ercius XPS-13 sourcetree <percius@lbl.gov>
2018-10-25 17:24:33 -0700

    allow importing all io using import ncempy.io

---
+++ ncempy/__init__.py
@@ -0,0 +1,8 @@
+'''This is the ncempy init
+
+#test
+from . import io
+from . import algo
+from . import eval
+
+'''
---
+++ ncempy/io/__init__.py
@@ -0,0 +1,4 @@
+from . import dm
+from . import ser
+from . import emd
+from . import mrc
