ca15f14cec42a04e14f439b0f59d6189856bfa6b
Ercius XPS-13 sourcetree <percius@lbl.gov>
2019-01-16 13:57:32 -0800

    Use h5py_cache to improve performance (10MB per file). Improve commenting. Add example. Fix some bugs. Tested on TEM and STEM data.

--- ncempy/io/emdVelox.py
+++ ncempy/io/emdVelox.py
@@ -4,41 +4,65 @@ Berkeley EMD data sets (see emd.py) instead.
 The reader for EMD Berkeley and Velox files will be combined in the near
 future once they are fully tested separately.
 
-Currently limited to only images. 
-
-TODO: Implement h5py_cache to open files with many images
-TODO: Test for list_data out of range
+Currently limited to only images. This file can not load spectra.
 '''
 
-import datetime
 import json
 
 import numpy as np
 import h5py
 
-#import h5py_cache
+import h5py_cache
 
 class fileEMDVelox:
-    '''Class to represent Velox EMD files
+    '''Class to represent Velox EMD files. It uses the h5py_cache module
+    to increase the default cache size from 1MB to 10MB. This significantly
+    improves file reading for EMDVelox files which are written with Fortran-
+    style ordering and a poor choice of chunking.
     
-    Parameters:
-        filename (str):    Name of the EMD file.
+    Parameters
+    ----------
+        filename : str
+            Name of the EMD file.
+    
+    Attributes
+    ----------
+        list_data : list
+            A list containing each h5py data group.
+        
+        file_hdl : h5py.File
+            The File handle from h5py.File.
+        
+        metaDataJSON : dict
+            The metadata for the most recently loaded data set.
+        
     
     Example
+    -------
+        Open an EMD Velox file of 1 image.
+        
+        >>> import ncempy.io as nio
+        >>> emd1 = nio.emdVelox.fileEMDVelox('1435 1.2 Mx STEM HAADF-DF4-DF2-BF.emd')
+        >>> print(emd1) # print informatio about the file
+        >>> im0, metadata0 = emd1.get_dataset(emd1.list_data[0])
+        >>> plt.imshow(im0, extent = (0,md1['pixelSize'][0],0,md1['pixelSize'][1]))
     '''
     
     def __init__(self, filename):
-        '''Init opening the file and finding all data groups.
+        '''Init opening the file and finding all data groups. Currently only
+        searches the /Data/Images group.
         
-        Parameters:
-            filename (str): 
-        
-        Currently only searches the Images group.
+        Parameters
+        ----------
+            filename :
+                The file path to load as a string.
         
         '''
         
-        ## necessary declaration in case something goes bad
+        ## necessary declaration in case something goes wrong
         self.file_hdl = None
+        self.metaDataJSON = None
+        self.list_data = None
         
         # check for string
         if not isinstance(filename, str):
@@ -46,7 +70,8 @@ class fileEMDVelox:
 
         # try opening the file
         try:
-            self.file_hdl = h5py.File(filename, 'r')
+            #self.file_hdl = h5py.File(filename, 'r')
+            self.file_hdl = h5py_cache.File(filename, 'r', chunk_cache_mem_size=10*1024**2)
         except:
             print('Error opening file for readonly: "{}"'.format(filename))
             raise
@@ -71,6 +96,7 @@ class fileEMDVelox:
     def __exit__(self,type,value,traceback):
         '''Implement python's with statment
         and close the file via __del__()
+        
         '''
         self.__del__()
         return None
@@ -83,20 +109,19 @@ class fileEMDVelox:
         out = 'EMD file contains {} data sets\n'.format(len(self.list_data))
         for ii, group in enumerate(self.list_data):
             md = self.parseMetaData(group)
-            out += 'Dataset #{} from detector: {}\n'.format(ii,md['detector'])
+            out += 'Dataset #{} from detector: {}\n'.format(ii,md['detectorName'])
         out += 'pixel size = ({0[0]:0.4f}, {0[1]:0.4f}) nm'.format(md['pixelSize'])
         return out
     
     def _find_groups(self):
-        '''Find all groups that contain data
+        '''Find all groups that contain data.
         
-        Note:
-            This currently only finds images
+        Note
+        ----
+            This currently only finds images.
         '''
         try:
-            #Get groups and images and metadata
-            f1Im = self.file_hdl['Data/Image']
-            #Get all of the groups in the Image group
+            # Get all of the groups in the Image group
             self.list_data = list(self.file_hdl['Data/Image'].values())
         except:
             self.list_data = []
@@ -105,31 +130,59 @@ class fileEMDVelox:
     def get_dataset(self, group):
         '''Get the data from a group and the associated metadata.
         
+        Parameters
+        ----------
+            group : HDF5 dataset or int
+                The link to the HDF5 dataset in the file or an integer for the 
+                number of the dataset. The list of datasets is held in the
+                list_data attribute populated on class init.
         '''
         # check input
+        try:
+            if type(group) is int:
+                group = self.list_data[group]
+        except IndexError:
+            raise IndexError('EMDVelox group #{} does not exist.'.format(group))
+        
         if not isinstance(group, h5py._hl.group.Group):
             raise TypeError('group needs to refer to a valid HDF5 group!')
 
-        data = group['Data'] #the full data set
+        data = np.squeeze(group['Data'][:]) #load the full data set
         metaData = self.parseMetaData(group)
         return (data,metaData)
     
     def parseMetaData(self,group):
-        '''Parse metadata in a data group.
+        '''Parse metadata in a data group. Determines the pixelSize and 
+        detector name. The EMDVelox data sets have extensive metadata
+        stored as a JSON type string.
         
-        Finds the pixelSize and detector name.
+        Parameters
+        ----------
+            group : h5py group
+                The h5py group to load the metadata from. The string is loaded
+                and parsed by the json module into a dictionary
+        
+        Returns
+        -------
+            md : dict
+                The JSON information returned as a python dictionary.
         
         '''
         md = {}
-        tempMetaData = group['Metadata'][:,0] #get the metadata
-        validMetaDataIndex = np.where(tempMetaData > 0) #find valid metadata
-        metaData = tempMetaData[validMetaDataIndex].tostring() #change to string
-        self.metaDataJSON = json.loads(metaData.decode('utf-8','ignore')) #load UTF-8 string as JSON and output dict
+        tempMetaData = group['Metadata'][:,0]
+        # Reduce to valid metadata
+        validMetaDataIndex = np.where(tempMetaData > 0) 
+        metaData = tempMetaData[validMetaDataIndex].tostring()
+        # Interpret as UTF-8 encoded characters and load as JSON
+        self.metaDataJSON = json.loads(metaData.decode('utf-8','ignore'))
+        # Pull out basic meta data about the images
         detectorName = self.metaDataJSON['BinaryResult']['Detector']
         pixelSizeX = float(self.metaDataJSON['BinaryResult']['PixelSize']['width'])*1e9 #change to nm
         pixelSizeY = float(self.metaDataJSON['BinaryResult']['PixelSize']['height'])*1e9 #change to nm
+        # Construct meta data dictionary
         md['pixelSize'] = (pixelSizeX,pixelSizeY)
-        md['detector'] = detectorName
+        md['pixelSizeUnit'] = ('nm','nm')
+        md['detectorName'] = detectorName
         
         return md
     
