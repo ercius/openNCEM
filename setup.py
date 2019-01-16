78b6d4076e7ad4ff6ca4d1ddd57a5162e575c6ca
Ercius XPS-13 sourcetree <percius@lbl.gov>
2019-01-16 14:31:22 -0800

    update dependencies and update version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.1',
+    version='1.4.0',
 
     description='openNCEM\'s Python Package',
     long_description=long_description,
@@ -36,7 +36,7 @@ setup(
     author_email='percius@lbl.gov',
 
     # Choose your license
-    license='GPLv3+',
+    license='GPLv3+','MIT'
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -73,7 +73,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py'],       
+    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py', 'h5py_cache'],       
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
