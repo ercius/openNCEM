e2acbca531e147787e2b343c2f987a874c47b6df
Zack Gainsforth <zsg@gainsforth.com>
2019-07-19 10:04:40 -0700

    Added documentation for edstomo and changed setup.py to add elam file through manifest.

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'ncempy/long_description.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+
+
 setup(
     name='ncempy',
 
@@ -63,7 +65,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'test', 'data']),
+    packages=find_packages(exclude=['contrib', 'docs', 'test']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -86,15 +88,16 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
+    # package_data={
+    #     'edstomo': ['Elam/ElamDB12.txt'],
+    # },
+    include_package_data=True,
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('ncempy', ['edstomo/Elam/ElamDB12.txt'])],
+    # data_files=[('ncempy', ['ncempy/edstomo/Elam/ElamDB12.txt'])],
     
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
