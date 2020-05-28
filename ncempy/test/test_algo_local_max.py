8edd7353dc13901bdd0b02492b885f8a4537305d
Peter Ercius razer <percius@lbl.gov>
2020-05-28 15:29:12 -0700

    convert old tests to pytests. Remove some tests like plotting and input testing to simplify the tests. These tests and parts of ncempy are not well maintained.

--- ncempy/test/test_algo_local_max.py
+++ ncempy/test/test_algo_local_max.py
@@ -2,85 +2,48 @@
 Tests for the algo.local_max module.
 """
 
-import unittest
+import pytest
+from pathlib import Path
+
 import numpy as np
-import matplotlib.pyplot as plt
-import os
 
 import ncempy.io.emd
 import ncempy.algo.local_max
 import ncempy.viz
 
 
-class test_localmax(unittest.TestCase):
+class TestLocalmax():
     """
     Test the localmax module on diffraction patterns.
     """
-    
-    def test_local_max(self):
-        '''
+
+    @pytest.fixture
+    def data_location(self):
+        # Get the location of the test data files
+        test_path = Path(__file__).resolve()
+        root_path = test_path.parents[1]
+        return root_path / Path('data')
+
+    def test_local_max(self, data_location):
+        """
         Test the local_maxima algorithm to be used for ring diffraction patterns.
-        '''
+        """
         
-        plt.close('all')        
-        show=True
-        
-        # get an image
-        femd = ncempy.io.emd.fileEMD('ncempy/test/resources/Pt_SAED_D910mm_single/Pt_SAED_D910mm_single.emd')
-        data, dims = femd.get_emdgroup(femd.list_emds[0])
-        
-        img = data[0,:,:]
-        dims = dims[1:3]
-        
-        
-        ## local_max
-        # not working
-        with self.assertRaises(TypeError):
-            points = ncempy.algo.local_max.local_max(42, 10, 600)
-        with self.assertRaises(TypeError):
-            points = ncempy.algo.local_max.local_max(img, 'ten', 600)
-        with self.assertRaises(TypeError):
-            points = ncempy.algo.local_max.local_max(img, 10, 'sixhundred')
-            
-        # no points detected
-        self.assertIsNone (ncempy.algo.local_max.local_max(img, 10, 100000))
-        
-        # working
-        points = ncempy.algo.local_max.local_max(img, 10, 600)
-        self.assertIsNotNone(points)
-        
-        
-        ## plot_points
-        # not working
-        with self.assertRaises(TypeError):
-            plot = ncempy.viz.plot_points(42, points)
-        with self.assertRaises(TypeError):
-            plot = ncempy.viz.plot_points(img, np.array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]))
-            
-        # px coords, non inverted
-        plot = ncempy.viz.plot_points(img, points, vminmax=(0.0, 0.2), show=show)
-        
-        
-        ## points_todim
-        # not working
-        with self.assertRaises(TypeError):
-            points = ncempy.algo.local_max.points_todim([0,1,2,3,5], dims)
-        with self.assertRaises(TypeError):
-            points = ncempy.algo.local_max.points_todim(points, dims[0])
-        
-        # convert points to real dim
-        point = ncempy.algo.local_max.points_todim((0,0), dims)
-        points = ncempy.algo.local_max.points_todim(points, dims)
-        
-        # real coords, inverted
-        plot = ncempy.viz.plot_points(img, points, vminmax=(0.0, 0.2), dims=dims, invert=True, show=show)
-        
-        
-        # wait for plots
-        if show:
-            plt.show()
-    
-    
-# to test with unittest runner
-if __name__ == '__main__':
-    unittest.main()
+        with ncempy.io.emd.fileEMD(data_location / Path('Pt_SAED_D910mm_single.emd')) as emd0:
+            data, dims = emd0.get_emdgroup(0)
+
+            # Data comes out as 3D, but its really 1D
+            img = data[0, :, :]
+            dims = dims[1:3]
+
+            # no points detected
+            no_points = ncempy.algo.local_max.local_max(img, 10, 100000)
+            assert no_points is None
+
+            # working
+            points = ncempy.algo.local_max.local_max(img, 10, 600)
+            assert points is not None
+
+            # convert points to real dim
+            point = ncempy.algo.local_max.points_todim(np.array((0, 0)), dims)
+            points = ncempy.algo.local_max.points_todim(points, dims)
