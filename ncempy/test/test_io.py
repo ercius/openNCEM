470adc9e6c3ea321e6c3bc15bbd39c109a7e8abe
Peter Ercius razer <percius@lbl.gov>
2020-05-27 17:59:48 -0700

    Start writing pytest tests. Started with general tests to the io package.

--- ncempy/test/test_io.py
+++ ncempy/test/test_io.py
@@ -1,29 +1,73 @@
-import ncempy.io as nio
+import pytest
+
 from pathlib import Path
+import numpy as np
+import ncempy.io as nio
 
-dPath = Path(r'C:\Users\linol\Data')
+# Todo: Add emd velox (small file needed)
+# Todo: Add ser tests and small file
 
-print('ser')
-fPath = Path('01_Si110_5images_1.ser')
-_ = nio.ser.serReader(dPath / fPath)
-print(_.keys())
 
-print('dm')
-fPath = Path('5_Te_15x83_ss=3nm_CL=245_alpha=p48_p06sec_no beamstop_bin4_300kV.dm4')
-_ = nio.dm.dmReader(dPath / fPath)
-print(_.keys())
+@pytest.fixture
+def data_location():
+    # Get the location of the test data files
+    test_path = Path(__file__).resolve()
+    root_path = test_path.parents[1]
+    return root_path / Path('data')
 
-print('emd')
-fPath = Path('TimeSeries_20.emd')
-_ = nio.emd.emdReader(dPath / fPath)
-print(_.keys())
+@pytest.fixture
+def write_mrc(data_location):
+    """Write out a small MRC data set. No need for data in repo
+    and this also tests the writing capability."""
+    nio.mrc.mrcWriter(data_location / Path('temp.mrc'),
+                      np.ones((10,11,12),dtype=np.float32),
+                      (1, 2, 3))
 
-print('emdVelox')
-fPath = Path('1435 1.2 Mx STEM HAADF-DF4-DF2-BF.emd')
-_ = nio.emdVelox.emdVeloxReader(dPath / fPath)
-print(_.keys())
+def test_emd_berkeley(data_location):
+    emd_path = data_location / Path('Acquisition_18.emd')
+    with nio.emd.fileEMD(emd_path) as emd0:
+        assert len(emd0.list_emds) == 1
 
-print('mrc')
-fPath = Path('AgNWweld_tomo2_1wire_115kx_160mmCL.mrc')
-_ = nio.mrc.mrcReader(dPath / fPath)
-print(_.keys())
+
+def test_dm3_2d(data_location):
+    dm_path = data_location / Path('dmTest_float32_nonSquare_diffPixelSize.dm3')
+    with nio.dm.fileDM(dm_path) as dm3:
+        dd = dm3.getDataset(0)
+        assert dd['data'].shape == (64, 128)
+
+
+def test_dm4_2d(data_location):
+    dm_path = data_location / Path('dmTest_float32_nonSquare_diffPixelSize.dm4')
+    with nio.dm.fileDM(dm_path) as dm4:
+        dd = dm4.getDataset(0)
+        assert dd['data'].shape == (64, 128)
+
+
+def test_dm3_3d(data_location):
+    dm_path = data_location / Path('dmTest_3D_int16_64,65,66.dm3')
+    with nio.dm.fileDM(dm_path) as dm3:
+        dd = dm3.getDataset(0)
+        assert dd['data'].shape == (66, 65, 64)
+
+
+def test_dm4_3d(data_location):
+    dm_path = data_location / Path('dmTest_3D_int16_64,65,66.dm4')
+    with nio.dm.fileDM(dm_path) as dm4:
+        dd = dm4.getDataset(0)
+        assert dd['data'].shape == (66, 65, 64)
+
+
+def test_dm3_spectrum_1d(data_location):
+    dm_path = data_location / Path('08_carbon.dm3')
+    with nio.dm.fileDM(dm_path) as dm4:
+        dd = dm4.getDataset(0)
+        dd['data'].shape = 2048
+
+
+def test_mrc(data_location, write_mrc):
+
+    assert (data_location / Path('temp.mrc')).exists() is True
+
+    with nio.mrc.fileMRC(data_location / Path('temp.mrc')) as mrc0:
+        dd = mrc0.getDataset()
+        assert dd['data'].shape == (10, 11, 12)
