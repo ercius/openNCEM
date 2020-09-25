c53e919c2099902b2313c22c63f8f86939f0a02f
Peter surface book 2 <peter.ercius@gmail.com>
2020-09-25 08:08:31 -0700

    distortion commenting

--- ncempy/algo/distortion.py
+++ ncempy/algo/distortion.py
@@ -9,13 +9,19 @@ import scipy.optimize
 def filter_ring(points, center, rminmax):
     """Filter points to be in a certain radial distance range from center.
 
-    Parameters:
-        points (np.ndarray):    Candidate points.
-        center (np.ndarray/tuple):    Center position.
-        rminmax (tuple):    Tuple of min and max radial distance.
+    Parameters
+    ----------
+        points : np.ndarray
+            Candidate points.
+        center : np.ndarray or tuple
+            Center position.
+        rminmax : tuple
+            Tuple of min and max radial distance.
 
-    Returns:
-        (np.ndarray):    List of filtered points, two column array.
+    Returns
+    -------
+        : np.ndarray
+            List of filtered points, two column array.
 
     """
     
@@ -52,12 +58,17 @@ def points_topolar(points, center):
 
     Can be either in pixel or real dim, but should be the same for points and center.
 
-    Parameters:
-        points (np.ndarray):    Positions as two column array.
-        center (np.ndarray/tuple):    Origin of the polar coordinate system.
+    Parameters
+    ----------
+        points : np.ndarray
+            Positions as two column array.
+        center : np.ndarray or tuple
+            Origin of the polar coordinate system.
 
-    Returns:
-        (np.ndarray):    Positions in polar coordinate system as two column array (r, theta).
+    Returns
+    -------
+        : np.ndarray
+            Positions in polar coordinate system as two column array (r, theta).
 
     """
     
@@ -85,32 +96,44 @@ def points_topolar(points, center):
 def residuals_center( param, data):
     """Residual function for minimizing the deviations from the mean radial distance.
 
-    Parameters:
-        param (np.ndarray):    The center to optimize.
-        data (np.ndarray):    The points in x,y coordinates of the original image.
+    Parameters
+    ----------
+        param : np.ndarray
+            The center to optimize.
+        data : np.ndarray
+            The points in x,y coordinates of the original image.
 
-    Returns:
-        (np.ndarray):   Residuals.
+    Returns
+    -------
+        : np.ndarray
+           Residuals.
 
     """
     
     # manually calculating the radii, as we do not need the thetas
     rs = np.sqrt( np.square(data[:,0]-param[0]) + np.square(data[:,1]-param[1]) )
     
-    return (rs-np.mean(rs))
+    return rs-np.mean(rs)
     
     
 def optimize_center(points, center, maxfev=1000, verbose=None):
     """Optimize the center by minimizing the sum of square deviations from the mean radial distance.
 
-    Parameters:
-        points (np.ndarray):    The points to which the optimization is done (x,y coords in org image).
-        center (np.ndarray/tuple):    Initial center guess.
-        maxfev (int):    Max number of iterations forwarded to scipy.optimize.leastsq().
-        verbose (bool):    Set to get verbose output.
+    Parameters
+    ----------
+        points : np.ndarray
+            The points to which the optimization is done (x,y coords in org image).
+        center : np.ndarray or tuple
+            Initial center guess.
+        maxfev : int
+            Max number of iterations forwarded to scipy.optimize.leastsq().
+        verbose : bool
+            Set to get verbose output.
 
-    Returns:
-        (np.ndarray):    The optimized center.
+    Returns
+    -------
+        : np.ndarray
+            The optimized center.
 
     """
 
@@ -128,9 +151,9 @@ def optimize_center(points, center, maxfev=1000, verbose=None):
         raise TypeError('Something wrong with the input!')
 
     # run the optimization
-    popt, flag = scipy.optimize.leastsq( residuals_center, center, args=(points), maxfev=maxfev)
+    popt, flag, _, _, _ = scipy.optimize.leastsq(residuals_center, center, args=points, maxfev=maxfev)
     
-    if flag not in [1,2,3,4]:
+    if flag not in [1, 2, 3, 4]:
         print('WARNING: center optimization failed.')
 
     if verbose:
@@ -139,20 +162,26 @@ def optimize_center(points, center, maxfev=1000, verbose=None):
     return popt
     
 
-def rad_dis( theta, alpha, beta, order=2 ):
+def rad_dis(theta, alpha, beta, order=2):
     """Radial distortion due to ellipticity or higher order distortion.
 
     Relative distortion, to be multiplied with radial distance.
 
-    Parameters:
-        theta (np.ndarray/float):    Angles at which to evaluate.
-        alpha (float):    Orientation of major axis.
-        beta (float):    Strength of distortion (beta = (1-r_min/r_max)/(1+r_min/r_max).
-        order (int):    Order of distortion.
+    Parameters
+    ----------
+        theta : np.ndarray
+            Angles at which to evaluate. Must be float.
+        alpha : float
+            Orientation of major axis.
+        beta : float
+            Strength of distortion (beta = (1-r_min/r_max)/(1+r_min/r_max).
+        order : int
+            Order of distortion.
 
-    Returns:
-        (np.ndarray/float):    Distortion factor.
-
+    Returns
+    -------
+        : np.ndarray
+            Distortion factor.
     """
     
     return (1.-np.square(beta))/np.sqrt(1.+np.square(beta)-2.*beta*np.cos(order*(theta+alpha)))
@@ -161,21 +190,26 @@ def rad_dis( theta, alpha, beta, order=2 ):
 def residuals_dis(param, points, ns):
     """Residual function for distortions.
 
-    Parameters:
-        param (np.ndarray):    Parameters for distortion.
-        points (np.ndarray):    Points to fit to.
-        ns (tuple):    List of orders to account for.
+    Parameters
+    ----------
+        param : np.ndarray
+            Parameters for distortion.
+        points : np.ndarray
+            Points to fit to.
+        ns : tuple
+            List of orders to account for.
 
-    Returns:
-        (np.ndarray):   Residuals.
-
+    Returns
+    -------
+        : np.ndarray
+           Residuals.
     """
 
-    est = param[0]*np.ones(points[:,1].shape)
+    est = param[0]*np.ones(points[:, 1].shape)
     for i in range(len(ns)):
-        est *=rad_dis( points[:,1], param[i*2+1], param[i*2+2], ns[i])
+        est *=rad_dis(points[:, 1], param[i*2+1], param[i*2+2], ns[i])
             
-    return points[:,0] - est 
+    return points[:, 0] - est
     
     
 def optimize_distortion(points, ns, maxfev=1000, verbose=False):
@@ -183,15 +217,21 @@ def optimize_distortion(points, ns, maxfev=1000, verbose=False):
 
     The orders in the list ns are first fitted subsequently and the result is refined in a final fit simultaneously fitting all orders.
 
-    Parameters:
-        points (np.ndarray):    Points to optimize to (in polar coords).
-        ns (tuple):    List of orders to correct for.
-        maxfev (int):    Max number of iterations forwarded to scipy.optimize.leastsq().
-        verbose (bool):    Set for verbose output.
+    Parameters
+    ----------
+        points : np.ndarray
+            Points to optimize to (in polar coords).
+        ns : tuple
+            List of orders to correct for.
+        maxfev : int
+            Max number of iterations forwarded to scipy.optimize.leastsq().
+        verbose : bool
+            Set for verbose output.
 
-    Returns:
-        (np.ndarray):    Optimized parameters according to ns.
-
+    Returns
+    -------
+        : np.ndarray
+            Optimized parameters according to ns.
     """
     
     try:
@@ -204,8 +244,7 @@ def optimize_distortion(points, ns, maxfev=1000, verbose=False):
         assert(len(ns)>=1)
     except:
         raise TypeError('Something wrong with the input!')
-        
-    
+
     # init guess for full fit
     init_guess = np.ones(len(ns)*2+1)
     init_guess[0] = np.mean(points[:,0])
@@ -220,9 +259,10 @@ def optimize_distortion(points, ns, maxfev=1000, verbose=False):
     # subsequently fit the orders
     for i in range(len(ns)):
         # optimize order to points_tmp
-        popt, flag = scipy.optimize.leastsq( residuals_dis, (init_guess[0], 0.1, 0.1), args=(points_tmp, (ns[i],)), maxfev=maxfev)
+        popt, flag, _, _, _ = scipy.optimize.leastsq(residuals_dis, np.array((init_guess[0], 0.1, 0.1)),
+                                                     args=(points_tmp, (ns[i],)), maxfev=maxfev)
         
-        if flag not in [1,2,3,4]:
+        if flag not in [1, 2, 3, 4]:
             print('WARNING: optimization of distortions failed.')
         
         # information
@@ -234,15 +274,15 @@ def optimize_distortion(points, ns, maxfev=1000, verbose=False):
         init_guess[i*2+2] = popt[2]
         
         # do correction
-        points_tmp[:,0] /= rad_dis(points_tmp[:,1], popt[1], popt[2], ns[i])
+        points_tmp[:, 0] /= rad_dis(points_tmp[:, 1], popt[1], popt[2], ns[i])
     
     # full fit    
     if verbose:
         print('starting the full fit:')    
     
-    popt, flag = scipy.optimize.leastsq( residuals_dis, init_guess, args=(points, ns), maxfev=maxfev)
+    popt, flag, _, _, _ = scipy.optimize.leastsq(residuals_dis, init_guess, args=(points, ns), maxfev=maxfev)
     
-    if flag not in [1,2,3,4]:
+    if flag not in [1, 2, 3, 4]:
         print('WARNING: optimization of distortions failed.')
     
     if verbose:
@@ -251,5 +291,3 @@ def optimize_distortion(points, ns, maxfev=1000, verbose=False):
             print('.. order={}, alpha={}, beta={}'.format(ns[i], popt[i*2+1], popt[i*2+2]))
 
     return popt
-    
-    
