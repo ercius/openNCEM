6b01536e54dae95d00d9b13f96666f464ec5f49a
Peter Ercius razer <percius@lbl.gov>
2022-02-03 17:57:43 -0800

    add import, add documentation

--- ncempy/algo/eels.py
+++ ncempy/algo/eels.py
@@ -1,5 +1,11 @@
-# EELS background fitting and analysis functions
+# Convenience EELS background fitting and analysis functions. Hyperspy and other dedicated packages are much more
+# powerful, but simple functions like these here can be used in cases where more simple or more complex analysis
+# can benefit from access to low level functionality. For example, a Gaussian-Lorentz background fit for low-loss
+# EELS is included which allows for pre- and post-edge values to be used in the fit.
+
 import numpy as np
+from scipy import optimize as opt
+
 from .gaussND import gauss1D
 from .gaussND import lorentz1D
 
@@ -27,6 +33,7 @@ def powerFit(sig, box):
         yy[yy < 0] = 0
         yy += 1
 
+        # TODO Replace polfit with numpy.Polynomial.fit
         pp = np.polyfit(np.log(eLoss), np.log(yy[box[0]:box[1]]), 1)  # log-log fit
     except:
         print('fitting problem')
@@ -40,17 +47,44 @@ def powerFit(sig, box):
         background = np.zeros_like(pixel_range)
     return background, pixel_range
 
-def pre_post_fit(energy_axis, spectra, pre, post, initial_parameters):
-    """Fit to pre and post edge. Most useufl for low loss EELS"""
+
+def pre_post_fit(energy_axis, spectra, pre, post, initial_parameters=(0, .15, .15, 0.5, 0)):
+    """ Fit a Gaussian-Lorentz to pre- and post-edge signals. This is useful for low loss EELS
+
+    The function is of the form:
+    GL = n * Gaussian(x0, sigma) + (1-n) * Lorentz(x0, w) + C
+
+    Parameters
+    ----------
+    energy_axis: ndarray, 1D
+        The energy loss values for each spectral intensity.
+    spectra: ndarray, 1D
+        The values of the spectra at the corresponding energy loss values.
+    pre: 2-tuple
+        Start and end pixels for the pre-edge fitting range (start, end)
+    post: 2-tuple
+        Start and end pixels for the post-edge fitting range (start, end)
+    initial_parameters: tuple
+        The initial guess for the parameters of the form (x0, sigma, w, n, C) as described in the function definition.
+        All values are in eV except n which is dimensionless but should be [0, 1]. Default initial parameters are for
+        a zero loss peak centered at 0 eV with a width of 0.15 eV, 50% mixing of the gaussian/lorentz and zero offset.
+
+    Returns
+    -------
+    : 2-tuple
+        A tuple contains two 1D ndarrays. The first is the energy loss values for the signal and the second is the
+        background subtracted signal.
+
+    """
     # Put the pre and post signals in the same arrays
     eLoss_fit = np.concatenate((energy_axis[pre[0]:pre[1]], energy_axis[post[0]:post[1]]))
     sig_fit = np.concatenate((spectra[pre[0]:pre[1]], spectra[post[0]:post[1]]))
 
-    # Powerlaw fit to the pre and postedge
+    # Gaussian-Lorentz fit to the pre- and post-edge
     GL = lambda x, x0, sigma, w, n, C: n * gauss1D(x, x0, sigma) + (1 - n) * lorentz1D(x, x0, w) + C
     pp, r = opt.curve_fit(GL, eLoss_fit, sig_fit, p0=initial_parameters, maxfev=20000)
 
-    # Create background using fit
+    # Create background using fitted values
     bgnd_eloss = np.linspace(energy_axis[pre[0]], energy_axis[post[1]], energy_axis[pre[0]:post[1]].shape[0])
     bgnd = GL(bgnd_eloss, *pp)
 
