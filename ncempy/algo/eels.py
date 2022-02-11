4d2cd5eff2249a410bf3acfa4e2e0ca39e92b0c4
Peter Ercius razer <percius@lbl.gov>
2022-02-11 11:13:36 -0800

    Add fucntions for subtracting a powerlaw background from a spectrum line scan or spectrum image, line scan or map intensity summing, and RGB image creation.

--- ncempy/algo/eels.py
+++ ncempy/algo/eels.py
@@ -33,7 +33,7 @@ def powerFit(sig, box):
         yy[yy < 0] = 0
         yy += 1
 
-        # TODO Replace polfit with numpy.Polynomial.fit
+        # TODO Replace polyfit with numpy.Polynomial.fit
         pp = np.polyfit(np.log(eLoss), np.log(yy[box[0]:box[1]]), 1)  # log-log fit
     except:
         print('fitting problem')
@@ -100,11 +100,91 @@ def adaptive_background():
     pass
 
 
-def map_background():
-    """Fit background to a SI map and subtract it."""
-    pass
+def map_background(spectra, bgnd_box):
+    """ Power law background subtraction to a set of spectra.
+
+    Parameters
+    ----------
+    spectra : ndarray, 2D or 3D
+        Spectra as a 2D array (line scan) with shape (N, energy_loss) or a 3D array (spectrum image) with shape
+        (N, M, energy_loss).
+
+    bgnd_box : 2-tuple of ints
+        The starting and ending point (in pixels) for the powerLaw background subtraction.
+
+    Returns
+    -------
+    : ndarray, 2D or 3D
+        The background subtracted spectra.
+    """
+    if spectra.ndim == 3:
+        mm = spectra.reshape((spectra.shape[0] * spectra.shape[1], spectra.shape[2]))
+    elif spectra.ndim == 2:
+        mm = spectra
+    else:
+        print('Incorrect spectra size')
+        return
+    mm_sub = np.zeros_like(mm)
+    for ii, sig in enumerate(mm):
+        bgnd, eLoss0 = powerFit(sig, bgnd_box)
+        mm_sub[ii, bgnd_box[0]:] = sig[bgnd_box[0]:] - bgnd
+
+    if spectra.ndim == 3:
+        mm_sub = mm_sub.reshape(*spectra.shape)
+
+    return mm_sub
 
 
-def signal_sum():
-    """Sum the signal within the desired region. Used to create compositional maps and line scans."""
-    pass
+def map_sum(spectra, sig_box):
+    """ Sum the signal in the spectrum between two pixel values. This will essentially produce a compositional map of
+    the element of interset.
+
+    Parameters
+    ----------
+    spectra : ndarray, 2D or 3D
+        Spectra as a 2D array (line scan) with shape (N, energy_loss) or a 3D array (spectrum image) with shape
+        (N, M, energy_loss).
+    sig_box : 2-tuple of ints
+        The starting and ending point (in pixels) for the signal summation.
+
+    Returns
+    -------
+    : ndarray, 1D or 2D
+        For a line scan the return is a 1D set of points and for a spectrum image the returned 2D array is a map.
+
+
+    """
+    sig_map = np.sum(spectra[..., sig_box[0]:sig_box[1]], axis=-1)
+    return sig_map
+
+
+def create_rgb(maps, normalize=True):
+    """ Create an image that combines the input maps into a color image. Each map will be used as the red, green, and
+    blue channels of the final image.
+
+    Parameters
+    ----------
+    maps : ndarray, 3D
+        A 3D array of shape (3, M, N) where the first two dimensions are the color maps and the thrid dimension
+        indicates the red, green, and blue channels in that order.
+    normalize : bool
+        Set the maximum pixel of each image to 1 for each channel independently. Default is True.
+
+    Returns
+    -------
+    : ndarray, 3D
+        A 3D array of float values. If you pass this to matplotlib.pyplot.imshow or write it to a PNG file it will be
+        displayed as a color image where the red, green, blue channels are properly mixed.
+    """
+    if maps.ndim != 3:
+        raise ValueError("map array must be 3-dimensional.")
+
+    rgb = np.zeros((maps[0].shape[0], maps[0].shape[1], 3), dtype=np.float32)
+    for ii, m in enumerate(maps):
+        if normalize:
+            m0 = m - m.min()
+            m0 = m0 / m0.max()
+        else:
+            m0 = m
+        rgb[:, :, ii] = m0
+    return rgb
