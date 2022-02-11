8f1876f492208b465edb37ccd6987e82ec2b8ed3
Peter Ercius razer <percius@lbl.gov>
2022-02-11 11:55:31 -0800

    more clean up

--- ncempy/algo/__init__.py
+++ ncempy/algo/__init__.py
@@ -2,6 +2,6 @@ from . import align
 from . import fourier_operations
 from . import moments
 from . import multicorr_funcs
-from .rebin import rebin
 from . import radial_profile
 from . import peak_find
+from .rebin import rebin
