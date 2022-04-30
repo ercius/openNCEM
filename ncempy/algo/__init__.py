9835684363be56fc727b3680219cd5eb7a4fb88b
Peter Ercius razer <percius@lbl.gov>
2022-04-30 14:38:48 -0700

    move moments into algo.__init__
    Make test_algo.py and remove tests for removed files

--- ncempy/algo/__init__.py
+++ ncempy/algo/__init__.py
@@ -4,7 +4,6 @@ from scipy import ndimage
 
 
 from . import fourier_operations
-from . import moments
 from . import multicorr_funcs
 from . import radial_profile
 from . import peak_find
@@ -289,3 +288,106 @@ def stack_align(stack, align_type='static', real_filter=1, k_filter=1, shift_fun
             jj = ii
 
     return aligned, shifts
+
+def moments(im, order=3):
+    """ Calculate the image raw moments.
+
+    Parameters
+    ----------
+        im : ndarray
+            A 2D array of the image to calculate the moments for
+
+        order : int, default=3
+            The order to calculate the moments to.
+
+    Returns
+    -------
+        : ndarray
+            An ndarray of (order+1, order+1)
+    """
+    order = int(order) + 1
+    XX, YY = np.mgrid[0:im.shape[0], 0:im.shape[1]]
+    mm = np.zeros((order, order))
+    for ii in range(order):
+        XXii = XX ** ii
+        for jj in range(order):
+            mm[ii, jj] = np.sum(XXii * YY ** jj * im.astype(np.float64))
+    return mm
+
+
+def moments_central(im, cent=None, order=3):
+    """ Calculate the image central moments.
+
+    Parameters
+    ----------
+        im : ndarray
+            A 2D array of the image to calculate the central moments for
+
+        cent : tuple, 2 values, optional
+            The centroid calculated from the image raw moments. If it
+            is not supplied then it is calculated by moments()
+
+        order : int, default = 3
+            The order to calculate the orders to.
+
+    Returns
+    -------
+        : ndarray
+            An ndarray of size (order + 1, order +1)
+
+    """
+    if not cent:
+        m = moments(im, order)
+        cent = centroid(m)
+
+    order = int(order) + 1
+
+    XX, YY = np.mgrid[-cent[0]:im.shape[0] - cent[0],
+                      - cent[1]:im.shape[1] - cent[1]]
+    XX = XX.astype('float64')
+    YY = YY.astype('float64')
+
+    mc = np.zeros((order, order))
+    for ii in range(order):
+        XXii = XX ** ii
+        for jj in range(order):
+            mc[ii, jj] = np.sum(XXii * YY ** jj * im.astype(np.float64))
+    return mc
+
+
+def centroid(m):
+    """ Find the centroid based on the raw moments.
+
+    Parameters
+    ----------
+        m : ndarray
+            The output from moments()
+
+    Returns
+    -------
+        : tuple, 2 elements (X,Y)
+            The position of the centroid as a 2-tuple
+    """
+    return m[1, 0] / m[0, 0], m[0, 1] / m[0, 0]
+
+
+def moment_angle(mc):
+    """ Calculate the orientation of the object from its central moments
+
+    Parameters
+    ----------
+        mc : ndarray
+            The array of the central moments. Output from moments_central
+
+    Returns
+    -------
+        : float
+            The angle in radians.
+
+    """
+    mu_20 = mc[2, 0] / mc[0, 0]  # - centroid[0]**2
+    mu_02 = mc[0, 2] / mc[0, 0]  # - centroid[1]**2
+    mu_11 = mc[1, 1] / mc[0, 0]  # - centroid[0]*centroid[1]
+
+    th = 0.5 * np.arctan2(2 * mu_11, mu_20 - mu_02)
+    return th
--- ncempy/test/test_algo.py
+++ ncempy/test/test_algo.py
@@ -8,7 +8,24 @@ import numpy as np
 import ncempy.io as nio
 from ncempy.eval.stack_align import stack_align
 from ncempy.algo import stack_align as stack_align_algo
+from ncempy.algo import rebin
+import ncempy.algo as nalgo
+from ncempy.algo import fourier_operations
 
+def test_rebin():
+    aa = np.ones((50, 50), dtype='<u2')
+
+    # Test sum and uint16
+    bb = rebin(aa, 2, funcType='sum')
+
+    assert bb[0, 0] == 4
+
+    aa = np.ones((50, 50), dtype='f')
+
+    # Test mean and float
+    bb = rebin(aa, 2, funcType='mean')
+
+    assert bb[0, 0] == 1.0
 
 @pytest.fixture
 def data_location():
@@ -71,3 +88,41 @@ def test_stack_align_cross(data_location):
     for sh_dynamic, known_shift in zip(shifts0, out_stack_shifts):
         assert sh_dynamic[0] == -known_shift[0]
         assert sh_dynamic[1] == -known_shift[1]
+
+
+def test_moments():
+    """Test on a rotated line"""
+
+    ang = 35  # degree
+    sh = (2, -2)
+
+    # Create a a simple line
+    aa = np.zeros((101, 101))
+    aa[50, 1:-1] = 1
+    aa = fourier_operations.rotateImage(aa, ang * np.pi / 180.)
+    aa = fourier_operations.shiftImage(aa, sh)
+
+    M = nalgo.moments(aa)
+    mc = nalgo.moments_central(aa)
+
+    c = nalgo.centroid(M)  # the center
+    print('centroid = {}'.format(c))
+    for mm, nn in zip(sh, c):
+        assert mm + 50 == round(nn)
+
+    th = nalgo.moment_angle(mc) + np.pi/2.  # in radians
+    assert ang == round(th * 180 / np.pi)
+
+
+def test_moments_dtype():
+    """Test that moments are calculated correctly with uint8 input dtype"""
+    # Create test image
+    YY, XX = np.meshgrid(np.linspace(0, 24, 25), np.linspace(0, 24, 25))
+    RR = np.sqrt((XX - 24)**2 + (YY - 24)**2)
+    RR = 255 * np.abs((RR / RR.max()) - 1)
+
+    c1 = nalgo.centroid(nalgo.moments(RR))
+    c2 = nalgo.centroid(nalgo.moments(RR.astype(np.uint8)))
+
+    assert int(c1[0]) == int(c2[0])
+    assert int(c1[1]) == int(c2[1])
