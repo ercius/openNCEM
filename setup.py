023a5530a7617c70a1d3bcaae6871a6cd59f76b5
Ercius Dell Precision <percius@lbl.gov>
2022-07-10 10:49:09 -0700

    bump version. Remove python 3.6 and add 3.9 as supported.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.9.0',
+    version='1.10.0',
 
     description='openNCEM\'s Python Package',
     long_description=long_description,
@@ -55,9 +55,9 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
