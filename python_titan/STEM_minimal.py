e56f732ce5762e2ae0546f45f3c06a19155d61da
Peter Ercius ncem-gauss jupyter <percius.lbl.gov>
2023-03-30 13:53:58 -0700

    update commenting

--- python_titan/STEM_minimal.py
+++ python_titan/STEM_minimal.py
@@ -1,19 +1,22 @@
 # A minimal script to test connecting to the microsocpe and TIA in STEM mode.
 # An image is acquired, but not saved.
 
-# Connect to FEI TEMScripting and TIA using COM
+# Connect to FEI TEMScripting and TIA using COM 
 from comtypes.client import CreateObject
 from comtypes.safearray import safearray_as_ndarray # get data across COM barrier fast
 
 def getPixelSize():
-    # Get the pixel calibration from TIA
+    """Get the pixel calibration from TIA
+    """
     window1 = TIA.ActiveDisplayWindow()
-    Im1 = window1.FindDisplay(window1.DisplayNames[0]) #returns an image display object
-    unit1 = Im1.SpatialUnit #returns SpatialUnit object
-    unitName = unit1.unitstring #returns a string (such as nm)
-    calX = Im1.image.calibration.deltaX #returns the x calibration in meters
+    Im1 = window1.FindDisplay(window1.DisplayNames[0]) # returns an image display object
+    unit1 = Im1.SpatialUnit # returns SpatialUnit object
+    unitName = unit1.unitstring # returns a string (such as nm)
+    calX = Im1.image.calibration.deltaX # returns the x calibration in meters
     calY = Im1.image.calibration.deltaY
-    print('pixel size = {:03}, {:03}'.format(calX,calY))
+    print('pixel size = {:03}, {:03}'.format(calX, calY))
+    return((calX, calY))
+
 _microscope = CreateObject('TEMScripting.Instrument')
 print("Connected to microscope")
 TIA = CreateObject('ESVision.Application')
@@ -23,9 +26,9 @@ print('Connected to TIA')
 Acq = _microscope.Acquisition
 Proj = _microscope.Projection
 Ill = _microscope.Illumination
-Stage = _microscope.Stage #FEI stage. Not needed for TEAM Stage
+Stage = _microscope.Stage 
 
-detector0 = Acq.Detectors(0) #older pythoncom versions might require square brackets []
+detector0 = Acq.Detectors(0) #old microscopes might require square brackets [] instead of ()
 # Add the detector
 Acq.AddAcqDevice(detector0)
 
@@ -34,15 +37,16 @@ myStemAcqParams.Binning = 8
 myStemAcqParams.ImageSize = 0 #_microscope.ACQIMAGESIZE_FULL
 myStemAcqParams.DwellTime = 1e-6
 Acq.Detectors.AcqParams = myStemAcqParams
-#Acq.RemoveAllAcqDevices()
 
 print(detector0.Info.Name)
 
-print('Acquire')
+print('Acquire image')
 acquiredImageSet = Acq.AcquireImages() 
 
+# This quickly retrieves the data across the COM barrier
+# and returns the data as a numpy ndarray
 with safearray_as_ndarray:
-    startIm = acquiredImageSet(0).AsSafeArray
+    image = acquiredImageSet(0).AsSafeArray
 
 getPixelSize()
 
