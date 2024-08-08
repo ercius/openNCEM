b1dc67cd02f95e1163842c5ebbe3a5e68b773274
Peter Ercius ncem-gauss jupyter <percius.lbl.gov>
2024-08-08 15:11:43 -0700

    allow h5py to be version 3 and numpy <2. Older h5py version 2 does not work.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.11.2',
+    version='1.11.3',
 
     description='openNCEM\'s Python Package',
     long_description=long_description,
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy<2', 'scipy', 'matplotlib', 'h5py<3'],
+    install_requires=['numpy<2', 'scipy', 'matplotlib', 'h5py'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
