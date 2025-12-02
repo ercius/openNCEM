95210d13a0262b53d9cb16359d7bd592bb15f81e
Peter Ercius ncem-gauss jupyter <percius.lbl.gov>
2025-12-02 11:33:28 -0800

    add tests with repo data

--- ncempy/test/test_io_dectris.py
+++ ncempy/test/test_io_dectris.py
@@ -40,17 +40,5 @@ class Testdectris:
     def test_str_input(self, data_location):
         file_path = data_location / Path('au_145mm_68kx_microprobe_01_master.h5')
         with ncempy.io.dectris.fileDECTRIS(str(file_path)) as f0:
-            assert f0.data_shap[0] = 64
+            assert f0.raw_shape[1] == 192
     
-    #def test_read_file(self, data_location):
-    #    file_path = data_location / Path('dectris_master.h5')
-    #    with ncempy.io.dectris.fileDECTRIS(file_path) as f0:
-    #        dd = f0.getDataset()
-    #        assert 'data' in dd
-    #        assert dd['data'].shape[0] == 2048
-    
-    # def test_dectrisReader(self, data_location):
-    #     file_path = data_location / Path('dectris_master.h5')
-    #     dd = ncempy.io.dectris.dectrisReader(file_path)
-    #     assert 'data' in dd
-    #     assert dd['data'].shape[0] == 2048
